b4f5a9729d95d14768ffbafbdfb39c88be7ff08c
Antonio Takev <tonitakev@gmail.com>
2023-12-02 19:55:48 +0100

    chore(cors): Configure CORS policy

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { createApp, startServer } from './index';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const app = createApp();
 startServer(app, port);
